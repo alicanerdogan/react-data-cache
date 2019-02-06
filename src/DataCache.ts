c45340311ca23ff395a400e1ddeb9c44df52dfdf
alicanerdogan <aerdogan07@gmail.com>
2019-02-06 23:03:46 +0100

    Updated deps

--- src/DataCache.ts
+++ src/DataCache.ts
@@ -1,5 +1,40 @@
 import React from "react";
-import { IDataCacheContext, IResponse, IDataCache, FetchArgs, GetKey, Invalidate, DataCacheHookOutput, IParsedResponse } from "../types";
+export interface IResponseState {
+  isLoading: boolean;
+  response?: IResponse;
+}
+
+export interface IResponse {
+  status: number;
+  data: string;
+}
+
+export interface IParsedResponse {
+  status: number;
+  data?: { [key: string]: unknown };
+}
+
+export interface IDataCache {
+  [key: string]: IResponseState;
+}
+
+export interface IDataCacheContext {
+  cache: IDataCache;
+  updateCache: (key: string, value: IResponse) => void;
+  setAsLoading: (key: string) => void;
+}
+
+export type FetchArgs = [RequestInfo, RequestInit | undefined];
+export type GetKey = (input: RequestInfo, init?: RequestInit) => string;
+export type Invalidate = (input: RequestInfo, init?: RequestInit) => boolean;
+export type DataCacheHookOutput = [IResponse | undefined, boolean, () => void];
+export type DataCacheHookParsedOutput = [
+  IParsedResponse | undefined,
+  boolean,
+  () => void
+];
+export type DataCacheHookParsedState = [IParsedResponse | undefined, boolean];
+
 
 const devTools =
   (window as any).__REDUX_DEVTOOLS_EXTENSION__ &&
